7b44a96f5d9bf9c525d92cacc935adac1d13b1ca
mazenfteha <mazn34903@gmail.com>
2023-12-17 20:14:33 -0800

    copy with pipe and pipeline stream

--- streams/copy/copy.js
+++ streams/copy/copy.js
@@ -1,6 +1,7 @@
 // 1- trying to copy file but wihout using streams
 // this is bad practice can not be use with huge files
 const fs = require("fs/promises");
+const { pipeline } = require('node:stream');
 
 // (async () => {
 //     console.time("copy")
@@ -18,25 +19,60 @@ const fs = require("fs/promises");
 by create a buff and keep write to it 
 */
 
+// (async () => {
+//     console.time("copy")
+
+//     const srcFile = await fs.open("./test.txt", "r")
+//     const destFile = await fs.open("./text-copy.txt","w")
+
+//     let bytesRead = -1;
+//     while(bytesRead !==0) {
+//         const readResult = await srcFile.read()
+//         bytesRead = readResult.bytesRead
+
+//         if(bytesRead !== 16384) {
+//             const indexOfNotFilled = readResult.buffer.indexOf(0);
+//             const newBuffer = Buffer.alloc(indexOfNotFilled)
+//             readResult.buffer.copy(newBuffer, 0, 0, indexOfNotFilled)
+//             destFile.write(newBuffer)
+//         } else {
+//             destFile.write(readResult.buffer)
+//         }
+//     }
+//     console.timeEnd("copy")
+// })()
+
+
+//Understanding piping
+//copy file with pipe method
+// (async () => {
+//     console.time("copy")
+
+//     const srcFile = await fs.open("./test.txt", "r")
+//     const destFile = await fs.open("./text-copy.txt","w")
+
+//     const readStream = srcFile.createReadStream();
+//     const writeStream = destFile.createWriteStream();
+
+//     readStream.pipe(writeStream)
+
+//     readStream.on('end', ()=> {
+//         console.timeEnd("copy")
+//     })
+// })()
+
+//using pipline for error handling
 (async () => {
     console.time("copy")
 
     const srcFile = await fs.open("./test.txt", "r")
     const destFile = await fs.open("./text-copy.txt","w")
 
-    let bytesRead = -1;
-    while(bytesRead !==0) {
-        const readResult = await srcFile.read()
-        bytesRead = readResult.bytesRead
+    const readStream = srcFile.createReadStream();
+    const writeStream = destFile.createWriteStream();
 
-        if(bytesRead !== 16384) {
-            const indexOfNotFilled = readResult.buffer.indexOf(0);
-            const newBuffer = Buffer.alloc(indexOfNotFilled)
-            readResult.buffer.copy(newBuffer, 0, 0, indexOfNotFilled)
-            destFile.write(newBuffer)
-        } else {
-            destFile.write(readResult.buffer)
-        }
-    }
-    console.timeEnd("copy")
+    pipeline(readStream, writeStream, (err) => {
+        console.log(err)
+        console.timeEnd("copy")
+    })
 })()
