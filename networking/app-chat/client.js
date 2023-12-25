b161699299402216a0fc66484e45cd635128a923
mazenfteha <mazn34903@gmail.com>
2023-12-25 10:20:27 -0800

    improving the UI of our console

--- networking/app-chat/client.js
+++ networking/app-chat/client.js
@@ -8,20 +8,49 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
+const clearLine = (dir) => {
+    return new Promise((resolve, reject) => {
+        process.stdout.clearLine(dir, () => {
+            resolve()
+        })
+    })
+}
+
+const moveCursor = (dx, dy) => {
+    return new Promise((resolve, reject) => {
+        process.stdout.moveCursor(dx, dy, () => {
+            resolve()
+        })
+    })
+
+}
+
 const question = util.promisify(rl.question).bind(rl);
 
 const socket = net.createConnection({ host: "127.0.0.1", port:3008 },async () => { //three way handshake 
     console.log('Connected to the server!')
 
-    const message =await question("Enter a message > ")
+    const ask = async () => {
+        const message =await question("Enter a message > ")
+        // move the cursor one line up
+        await moveCursor(0, -1)
+        // clear the current line that the cursor is in
+        await clearLine(0)
+        // send the message to the server
+        socket.write(message)
+    }
 
-    // send the message to the server
-    socket.write(message)
+    ask()
+
+    socket.on('data', async (data) => {
+        console.log();
+        await moveCursor(0, -1)
+        await clearLine(0)
+        console.log(data.toString('utf-8'))
+        ask()
+    })
 }) 
 
-socket.on('data',(data) => {
-    console.log(data.toString('utf-8'))
-})
 
 socket.on('end', () => {
     console.log('Connection was gracefully ended by the server');
