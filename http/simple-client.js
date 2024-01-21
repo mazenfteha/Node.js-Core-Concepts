e49c4541b483b3dd244e3e904a139f96832e38aa
mazenfteha <mazn34903@gmail.com>
2024-01-20 21:06:14 -0800

    Create simple HTTP server and client, send request and recive a response

---
+++ http/simple-client.js
@@ -0,0 +1,40 @@
+const http = require("node:http");
+
+const agent = new http.Agent({ keepAlive: true })
+
+const request = http.request({
+    agent: agent,
+    hostname: "localhost",
+    port: 8050,
+    method: "POST",
+    path: "/create-post",
+    headers: {
+        "Content-Type": "application/json",
+        "name":"mazen"
+    }
+})
+
+request.on("response", (response) => {
+    console.log("--------- STATUS: ---------");
+    console.log(response.statusCode);
+
+    console.log("--------- HEADERS: ---------");
+    console.log(response.headers);
+
+    console.log("--------- BODY: ---------");
+    response.on('data', (chunk) => {
+        console.log(chunk.toString('utf-8'))
+    })
+
+    response.on('end', () => {
+        console.log('no more responses');
+    })
+})
+
+request.end(JSON.stringify({ 
+    title: "hello world example with every Node.js web framework",
+    lang: "Node.js", 
+    category: "Backend development",
+    body: "This is some text and more and more" 
+}))
+
---
+++ http/simple-server.js
@@ -0,0 +1,38 @@
+const http = require("node:http");
+
+const server = http.createServer();
+
+server.on("request", (request, response) => {
+    console.log("--------- METHOD: ---------");
+    console.log(request.method);
+
+    console.log("--------- URL: ---------");
+    console.log(request.url);
+
+    console.log("--------- HEADERS: ---------");
+    console.log(request.headers);
+
+    const name = request.headers.name 
+
+    console.log("--------- BODY: ---------");
+
+    let data = "";
+
+    request.on("data", (chunk) => {
+        data += chunk.toString()
+    });
+
+    request.on('end', () => {
+        data = JSON.parse(data)
+
+        console.log(data)
+        console.log(`Author name is : ${name}`)
+
+        response.writeHead(200, {"Content-Type": "application/json"});
+        response.end(JSON.stringify({ message: `Post with title ${data.title} was created by ${name}` }))
+    })
+});
+
+server.listen(8050, () => {
+    console.log("Server listening on http://localhost:8050");
+});
