8b7ac176f65ad80d9c9133cbbb23b70253d589f5
mazenfteha <mazn34903@gmail.com>
2024-02-01 15:49:20 -0800

    add cookies for authentication

--- http/poster/server.js
+++ http/poster/server.js
@@ -14,18 +14,23 @@ const POSTS = [
         userId: 1,
     }
 ]
+// { userId:1 , token: 2165416}
+const SESSTIONS = []
 
-const PORT = 9001;
+const PORT = 9000;
 
 const server = new Fteha();
 
 //---------------Files Routes-------//
 server.route('get', '/', (req, res) => {
-    console.log('server-1 is now handling this request')
     res.sendFile('./public/index.html', "text/html")
 })
 
 server.route('get', '/login', (req, res) => {
+    res.sendFile('./public/index.html', "text/html")
+})
+
+server.route('get', '/profile', (req, res) => {
     res.sendFile('./public/index.html', "text/html")
 })
 
@@ -38,15 +43,8 @@ server.route('get', '/scripts.js', (req, res) => {
 })
 
 //---------------JSON Routes-------//
-server.route('get', '/api/posts', (req, res) => {
-    const posts = POSTS.map((post) => {
-        const user = USERS.find((user) => user.id === post.userId)
-        post.author = user.name;
-        return post;
-    })
-    res.status(200).json(posts)
-})
 
+// Log a users in and give them a token
 server.route('post', '/api/login', (req, res) => {
     let body = "";
     req.on('data', (chunk) => {
@@ -57,10 +55,17 @@ server.route('post', '/api/login', (req, res) => {
         
         const username = body.username
         const password = body.password
-
+        
         // Check if the user exists
         const user = USERS.find((user) => user.username === username)
         if(user && user.password === password) {
+
+            const token = Math.floor(Math.random() * 10000000).toString()
+            res.setHeader("Set-cookie", `token=${token}; Path=/;`)
+            // Save the generated token
+            SESSTIONS.push({userId:user.id, token:token})
+            console.log(SESSTIONS)
+
             res.status(200).json({ message: "Logged in successfuly" })
         } else {
             res.status(401).json({ error: "Invalid username or password" })
@@ -68,6 +73,36 @@ server.route('post', '/api/login', (req, res) => {
     })
 })
 
+// Log a user out
+server.route('delete', '/api/logout', (req, res) => {})
+
+// Send user info
+server.route('get', '/api/user', (req, res) => {
+    const token = req.headers.cookie.split("=")[1];
+    const session = SESSTIONS.find((session) => session.token === token)
+    if(session) {
+        // Send the user's profile info
+        const user = USERS.find((user) => user.id === session.userId)
+        res.json({ username: user.name, name: user.name })
+    }else {
+        res.status(401).json({ error: "Unauthoized" })
+    }
+})
+
+// update a profile
+server.route('put', '/api/user', (req, res) => {})
+
+// create a post
+server.route('post', '/api/posts', (req, res) => {})
+
+server.route('get', '/api/posts', (req, res) => {
+    const posts = POSTS.map((post) => {
+        const user = USERS.find((user) => user.id === post.userId)
+        post.author = user.name;
+        return post;
+    })
+    res.status(200).json(posts)
+})
 
 server.listen(PORT, () => {
     console.log(`server running on fteha framework on port ${PORT}`)
