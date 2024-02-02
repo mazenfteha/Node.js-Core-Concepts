80c5efd1dfd8b45102c728d74b31a1b4d7e8f47a
mazenfteha <mazn34903@gmail.com>
2024-02-02 15:06:24 -0800

    Add middleware support to fteha.js

--- http/fteha.js
+++ http/fteha.js
@@ -6,6 +6,7 @@ class Fteha {
     constructor() {
         this.server = http.createServer()
         this.routes = {}
+        this.middleware = [];
 
         this.server.on('request', (req, res) => {
 
@@ -31,18 +32,46 @@ class Fteha {
             }
 
 
-            // if the routes objects does not have a key of req.method + req.url
-            if(!this.routes[req.method.toLowerCase() + req.url]) {
-                return res.status(404).json({ error: `Cannot ${req.method} ${req.url}`})
-            }
+            // Run all the midddleware functions before we run routes
+            // this.middleware[0](req, res, () => {
+            //     this.middleware[1](req, res, () => {
+            //         this.middleware[2](req, res, () => {
+            //             this.routes[req.method.toLowerCase() + req.url](req, res)
+            //         })
+            //     })
+            // })
 
-            this.routes[req.method.toLowerCase() + req.url](req, res)
+
+            // Run all the middleware functions before we run the corresponding route
+            const runMiddleware = (req, res, middleware, index) => {
+                // Out exit point...
+                if (index === middleware.length) {
+                    // If the routes object does not have a key of req.method + req.url, return 404
+                    if (!this.routes[req.method.toLocaleLowerCase() + req.url]) {
+                        return res
+                            .status(404)
+                            .json({ error: `Cannot ${req.method} ${req.url}` });
+                    }
+
+                    this.routes[req.method.toLowerCase() + req.url](req, res);
+                } else {
+                    middleware[index](req, res, () => {
+                        runMiddleware(req, res, middleware, index + 1);
+                    });
+                }
+            };
+
+            runMiddleware(req, res, this.middleware, 0);
         })
     }
 
 
     route(method, path, cb){
         this.routes[method + path] = cb
+    }
+
+    beforeEach(cb) {
+        this.middleware.push(cb)
     }
 
     listen (port, cb) {
--- http/poster/server.js
+++ http/poster/server.js
@@ -21,18 +21,68 @@ const PORT = 9000;
 
 const server = new Fteha();
 
+//Middlewares
+// For authentication
+server.beforeEach((req, res, next) => {
+    const routesToAuthenticate = [
+        "GET /api/user",
+        "PUT /api/user",
+        "POST /api/posts",
+        "DELETE /api/logout",
+    ];
+
+    if (routesToAuthenticate.indexOf(req.method + " " + req.url) !== -1) {
+        // If we have a token cookie, then save the userId to the req object
+        if (req.headers.cookie) {
+            const token = req.headers.cookie.split("=")[1];
+
+            const session = SESSTIONS.find((session) => session.token === token);
+            if (session) {
+                req.userId = session.userId;
+                return next();
+            }
+        }
+
+        return res.status(401).json({ error: "Unauthorized" });
+    } else {
+        next();
+    }
+});
+
+
+const parseJSON = (req, res, next) => {
+    // This is only good for bodies that their size is less than the highWaterMark value
+    if (req.headers["content-type"] === "application/json") {
+        let body = "";
+        req.on("data", (chunk) => {
+            body += chunk.toString("utf-8");
+        });
+
+        req.on("end", () => {
+            body = JSON.parse(body);
+            req.body = body;
+            return next();
+        });
+    } else {
+        next();
+    }
+};
+
+// For parsing JSON body
+server.beforeEach(parseJSON);
+
+// For different routes that need the index.html file
+server.beforeEach((req, res, next) => {
+    const routes = ["/", "/login", "/profile", "/new-post"];
+
+    if (routes.indexOf(req.url) !== -1 && req.method === "GET") {
+        return res.status(200).sendFile("./public/index.html", "text/html");
+    } else {
+        next();
+    }
+});
+
 //---------------Files Routes-------//
-server.route('get', '/', (req, res) => {
-    res.sendFile('./public/index.html', "text/html")
-})
-
-server.route('get', '/login', (req, res) => {
-    res.sendFile('./public/index.html', "text/html")
-})
-
-server.route('get', '/profile', (req, res) => {
-    res.sendFile('./public/index.html', "text/html")
-})
 
 server.route('get', '/styles.css', (req, res) => {
     res.sendFile('./public/styles.css', "text/css")
@@ -45,48 +95,37 @@ server.route('get', '/scripts.js', (req, res) => {
 //---------------JSON Routes-------//
 
 // Log a users in and give them a token
-server.route('post', '/api/login', (req, res) => {
-    let body = "";
-    req.on('data', (chunk) => {
-        body += chunk.toString('utf-8')
-    })
-    req.on('end', () => {
-        body = JSON.parse(body);
-        
-        const username = body.username
-        const password = body.password
-        
-        // Check if the user exists
-        const user = USERS.find((user) => user.username === username)
-        if(user && user.password === password) {
+server.route("post", "/api/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
-            const token = Math.floor(Math.random() * 10000000).toString()
-            res.setHeader("Set-cookie", `token=${token}; Path=/;`)
-            // Save the generated token
-            SESSTIONS.push({userId:user.id, token:token})
-            console.log(SESSTIONS)
+    // Check if the user exists
+    const user = USERS.find((user) => user.username === username);
 
-            res.status(200).json({ message: "Logged in successfuly" })
-        } else {
-            res.status(401).json({ error: "Invalid username or password" })
-        }
-    })
-})
+    // Check the password if the user was found
+    if (user && user.password === password) {
+        // At this point, we know that the client is who they say they are
+
+        // Generate a random 10 digit token
+        const token = Math.floor(Math.random() * 10000000000).toString();
+
+        // Save the generated token
+        SESSTIONS.push({ userId: user.id, token: token });
+
+        res.setHeader("Set-Cookie", `token=${token}; Path=/;`);
+        res.status(200).json({ message: "Logged in successfully!" });
+    } else {
+        res.status(401).json({ error: "Invalid username or password." });
+    }
+});
 
 // Log a user out
 server.route('delete', '/api/logout', (req, res) => {})
 
 // Send user info
 server.route('get', '/api/user', (req, res) => {
-    const token = req.headers.cookie.split("=")[1];
-    const session = SESSTIONS.find((session) => session.token === token)
-    if(session) {
-        // Send the user's profile info
-        const user = USERS.find((user) => user.id === session.userId)
-        res.json({ username: user.name, name: user.name })
-    }else {
-        res.status(401).json({ error: "Unauthoized" })
-    }
+    const user = USERS.find((user) => user.id === req.userId);
+    res.json({ username: user.username, name: user.name });
 })
 
 // update a profile
